97253decf122c1d4996882b22ceb0ee8fc92fdf2
Michael <greenmr95@hotmail.com>
2022-11-25 04:12:41 +0000

    breedFetcher accepts CLA and returns appropriate errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,18 @@
-const request = require('request')
 
+const request = require('request');
+
+let breed = process.argv[2];
+let url = "https://api.thecatapi.com/v1/breeds/search?q=" + breed;
+
+request(url, (error, response, body) => {
+  try {
+    const data = JSON.parse(body);
+    if (typeof data[0] === "undefined") {
+      console.log("Breed Not Found");
+    } else {
+      console.log(data[0]);
+    }
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+});
